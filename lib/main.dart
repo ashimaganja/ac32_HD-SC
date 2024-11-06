c07d7f6878e6155fad1a51b9da20fc49992286c0
ashimaganja <ashimaganja98@gmail.com>
2024-11-06 16:49:42 -0500

    The log in page cloned

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+
 import 'package:flutter/material.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -39,14 +29,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +37,217 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final _formKey = GlobalKey<FormState>(); //creating a key for a form
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+   bool _isObscured = true;
+
+  @override
+  void initState(){
+    super.initState();
+    _isObscured = true;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.blue,
+      //
+      //   title: Text(widget.title),
+      // ),
+      backgroundColor:  const Color.fromARGB(150, 26, 37, 101),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Stack(
+              children: [
+                Container(
+                  height: 400,
+                  width: 600,
+                  color: Colors.blue,
+                  child:Form(
+                    key: _formKey,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 100, vertical:60),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding:
+                            const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                            child: TextFormField(
+                              controller: emailController,
+                              decoration: const InputDecoration(
+                                  hintText: "Email",
+                                hintStyle: const TextStyle(
+                                  color: Colors.black, // Change to your desired color
+                                  fontSize: 16, // Optional: Adjust font size
+                                ),
+
+                                  icon:  Icon(Icons.person, color: Colors.white,),
+                              fillColor: Colors.white,
+                              filled: true,
+
+                              ),
+
+                              validator: (value){
+                                if(value == null || value.isEmpty){
+                                  return 'Please enter your email';
+                                }
+                                return null;
+                              },
+                            ),
+
+                          ),
+                          Padding(padding:
+                          const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                            child: TextFormField(
+                              controller: passwordController,
+                              obscureText: _isObscured,
+                              decoration: InputDecoration(
+                                suffixIcon: IconButton(
+                                  padding: const EdgeInsetsDirectional.only(end: 12.0),
+                                  icon: _isObscured ? const Icon(Icons.visibility, color: Colors.black,) : const Icon(Icons.visibility_off, color: Colors.black,),
+                                  onPressed: (){
+                                    setState(() {
+                                      _isObscured =! _isObscured;
+                                    });
+                                  },),
+                                hintText: "Password",
+                                hintStyle: const TextStyle(
+                                  color: Colors.black, // Change to your desired color
+                                  fontSize: 16, // Optional: Adjust font size
+                                ),
+
+                                icon: const Icon(Icons.lock,color: Colors.white,),
+                                fillColor: Colors.white,
+                                filled: true,
+                              ),
+                              validator: (value){
+                                if(value == null || value.isEmpty){
+                                  return 'Please enter your password';
+                                }
+                                else{
+                                  return null;
+                                }
+                              },
+
+                            ),),
+
+                          Padding(padding:
+                          const EdgeInsets.symmetric(horizontal: 8, vertical: 25),
+                            child: Center(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+
+                                children:<Widget> [
+                               ElevatedButton(
+                                 style: ElevatedButton.styleFrom(
+                                   minimumSize: const Size(15, 30),
+                                   // width: 100, height: 40
+                                 ),
+                                      onPressed: (){
+                                        exit(0);
+                                      },
+                                      child: const Align(
+                                        alignment: Alignment.center,  // This ensures the text is centered
+                                        child: Text('Exit'),
+                                      ),
+                                    ),
+                                 const SizedBox(
+                                   width: 10,
+                                 ),
+                                  ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      minimumSize: const Size(15, 30),
+                                      // width: 100, height: 40
+                                    ),
+                                      onPressed: (){
+                                        if(_formKey.currentState!.validate()){
+                                          //Navigate the user to the home page
+
+                                          // if(emailController.text == "gmail" && passwordController.text == "catandmouse"){
+                                          //   Navigator.push(context,
+                                          //     MaterialPageRoute(builder:
+                                          //         (context) => HomePage(email:emailController.text,)
+                                          //     ),);
+                                          //
+                                          // } else{
+                                          //   ScaffoldMessenger.of(context).showSnackBar(
+                                          //       const SnackBar(content: Text('Invalid Credential'))
+                                          //   );
+                                          // }
+
+                                        }else{
+                                          ScaffoldMessenger.of(context).showSnackBar(
+                                              const SnackBar(content: Text('Please fill input'))
+                                          );
+                                        }
+                                      },
+                                      child: const Align(
+                                    alignment: Alignment.center,  // This ensures the text is centered
+                                    child: Text('Submit'),
+                                  ),
+                                    ),
+
+                                ],
+                              ),
+
+                                // child:ElevatedButton(
+                                //   onPressed: (){
+                                //     if(_formKey.currentState!.validate()){
+                                //       //Navigate the user to the home page
+                                //
+                                //       // if(emailController.text == "gmail" && passwordController.text == "catandmouse"){
+                                //       //   Navigator.push(context,
+                                //       //     MaterialPageRoute(builder:
+                                //       //         (context) => HomePage(email:emailController.text,)
+                                //       //     ),);
+                                //       //
+                                //       // } else{
+                                //       //   ScaffoldMessenger.of(context).showSnackBar(
+                                //       //       const SnackBar(content: Text('Invalid Credential'))
+                                //       //   );
+                                //       // }
+                                //
+                                //     }else{
+                                //       ScaffoldMessenger.of(context).showSnackBar(
+                                //           const SnackBar(content: Text('Please fill input'))
+                                //       );
+                                //     }
+                                //   },
+                                //   child: const Text('Submit'),
+                                // )
+
+                            ),)
+
+                        ],
+                      ),
+                    ),
+                  )
+                  ,
+                ),
+                const Positioned(
+                  top: 8,
+                  left: 8,
+                  child: Text(
+                    'Please log in',
+                    style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                ),
+              ],
+            )
+
+
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
     );
   }
 }
